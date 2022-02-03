47917ff3b437ff45065cf930c04631fa0c1cdc25
miro.joos <miro.joos@CPT.local>
2022-02-03 11:37:14 +0100

    applicativo + primo design

---
+++ 5_Sito o applicativo/RidersAdventure/RidersAdventure/src/components/Event.vue
@@ -0,0 +1,57 @@
+<template>
+    <div id="event">
+        <h1>{{ title }}</h1>
+        <div>
+            <p> -- map -- </p>
+            <p>{{ description }}</p>
+            <p>{{ meeting }}</p>
+            <p>{{ date }}</p>
+            <button @click="takePart()">TAKE PART</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        id: {
+            type: String,
+            default: ""
+        },
+        title: {
+            type: String,
+            default: ""
+        },
+        description: {
+            type: String,
+            default: ""
+        },
+        meeting: {
+            type: String,
+            default: ""
+        },
+        date: {
+            type: String,
+            default: ""
+        }
+    },
+    methods: {
+        takePart() {
+            // 'this' is used to access props and local variables
+            alert("You have taking part at event " + this.id)
+        }
+    }
+}
+</script>
+
+<style>
+    #event {
+        background: black;
+        color: white;
+        display: inline-block;
+        padding: 20px;
+        margin: 20px;
+        border-radius: 10px;
+        width: 20%;
+    }
+</style>
---
+++ 5_Sito o applicativo/RidersAdventure/RidersAdventure/src/components/EventsList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <Event v-for="event, itemIndex in events" 
+        :key="itemIndex"
+        :title="event.title"
+        :description="event.description"
+        :date="event.date" />
+        <button @click="logEvents">LOG EVENTS</button>
+    </div>
+</template>
+
+<script>
+import Event from "./Event.vue"
+
+export default {
+    components: {
+        Event
+    },
+    props: {
+        events: {
+            type: Array,
+            default: () => [] // Empty array
+        }
+    },
+    methods: {
+        logEvents() {
+            console.log(this.events);
+        }
+    }
+}
+</script>
